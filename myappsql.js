7eb8e0bac575a297f207b3946c318a7eafeb31ab
Prema Palanisamy <prema.palanisamy@aspiresys.com>
2024-08-21 22:34:30 +0530

    docker file added

--- myappsql.js
+++ myappsql.js
@@ -1,21 +1,33 @@
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 
-async function main() {
-    const pool = mysql.createPool({
-        socketPath: '/cloudsql/durable-circle-433106-g9:us-central1:spacevote-instance', // Ensure this path is correct
-        user: 'prema', // Use environment variables for sensitive information
-        password: 'Admin@123', // Use environment variables for sensitive information
-        database: 'votecast' // Use environment variables for sensitive information
-    });
+// async function main() {
+//     const pool = mysql.createPool({
+//         socketPath: '/cloudsql/durable-circle-433106-g9:us-central1:spacevote-instance', // Ensure this path is correct
+//         user: 'prema', // Use environment variables for sensitive information
+//         password: 'Admin@123', // Use environment variables for sensitive information
+//         database: 'votecast' // Use environment variables for sensitive information
+//     });
 
-    try {
-        const [rows, fields] = await pool.query('SELECT * FROM vote_table');
-        console.log('Query results:', rows);
-    } catch (err) {
-        console.error('Error executing query:', err.stack);
-    } finally {
-        await pool.end();
-    }
-}
+//     try {
+//         const [rows, fields] = await pool.query('SELECT * FROM vote_table');
+//         console.log('Query results:', rows);
+//     } catch (err) {
+//         console.error('Error executing query:', err.stack);
+//     } finally {
+//         await pool.end();
+//     }
+// }
 
-main();
+// main();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 81;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
