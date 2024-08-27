485101ffbc3f31777ccda347b5f7d6a8dd53479c
prema-palanisamy <prema.palanisamy@aspiresys.com>
2024-08-27 21:24:27 +0530

    main file changedd

--- myappsql.js
+++ myappsql.js
@@ -109,7 +109,7 @@ const port = process.env.PORT || 8080;
 
 app.get('/',(req, res) => {
   console.log("hello")
-  res.send("success")
+  res.send("Welcome to my world")
 })
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
